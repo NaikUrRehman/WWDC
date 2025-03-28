98224b0ae6635757fbe66eea08d88fc2f7eec6b5
NaikUrRehman <paknaik.101@gmail.com>
2025-03-28 11:57:45 +0500

    Initial Commit

---
+++ WWDC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WWDC
+//
+//  Created by user on 28/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WWDC/WWDCApp.swift
@@ -0,0 +1,17 @@
+//
+//  WWDCApp.swift
+//  WWDC
+//
+//  Created by user on 28/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct WWDCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
